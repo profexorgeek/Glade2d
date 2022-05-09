2743b3aaa053c1d2b0f3b1233fe7f2e21b626585
Justin Johnson <justin.d.johnson@gmail.com>
2022-05-08 19:53:20 -0600

    Moved drawing code into a Renderer class

--- Glade2d/Graphics/Renderer.cs
+++ Glade2d/Graphics/Renderer.cs
@@ -1,7 +1,11 @@
-﻿using Meadow.Foundation.Graphics;
+﻿using Glade2d.Services;
+using Meadow;
+using Meadow.Foundation;
+using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Graphics.Buffers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Glade2d.Graphics
@@ -11,9 +15,76 @@ namespace Glade2d.Graphics
         MicroGraphics graphicsDriver;
         Dictionary<string, IDisplayBuffer> textures = new Dictionary<string, IDisplayBuffer>();
 
+        Color BackgroundColor { get; set; } = Color.Black;
+        Color TransparentColor { get; set; } = Color.Magenta;
+
         public Renderer(MicroGraphics graphics)
         {
+            LogService.Log.Trace("Initializing Renderer");
             graphicsDriver = graphics;
+            graphicsDriver.Clear(true);
+        }
+
+        public void Clear(bool sendToDevice = false)
+        {
+            graphicsDriver.Clear(sendToDevice);
+            graphicsDriver.DrawRectangle(0, 0, 240, 240, BackgroundColor, true);
+        }
+
+        public void RenderFrame(int originX, int originY, Frame frame)
+        {
+            if (!textures.ContainsKey(frame.TextureName))
+            {
+                LoadTexture(frame.TextureName);
+            }
+
+            var buffer = textures[frame.TextureName];
+
+            for (var x = frame.X; x < frame.X + frame.Width; x++)
+            {
+                for (var y = frame.Y; y < frame.Y + frame.Height; y++)
+                {
+                    var pixel = buffer.GetPixel(x, y);
+                    if (!pixel.Equals(TransparentColor))
+                    {
+                        graphicsDriver.DrawPixel(originX + x, originY + y, pixel);
+                    }
+                }
+            }
+        }
+
+        public void DrawBuffer()
+        {
+            graphicsDriver.Show();
+        }
+
+        public void LoadTexture(string name)
+        {
+            var buffer = LoadBitmapFile(name);
+            textures.Add(name, buffer);
+        }
+
+        public void UnloadTexture(string name)
+        {
+            textures.Remove(name);
+        }
+
+        IDisplayBuffer LoadBitmapFile(string name)
+        {
+            LogService.Log.Trace($"Attempting to LoadBitmapFile: {name}");
+            var filePath = Path.Combine(MeadowOS.FileSystem.UserFileSystemRoot, name);
+
+            try
+            {
+                var img = Image.LoadFromFile(filePath);
+                return img.DisplayBuffer;
+            }
+            catch(Exception ex)
+            {
+                LogService.Log.Error($"Failed to load {filePath}: The file should be a 24bit bmp, in the root directory, BuildAction = Content, and Copy if Newer!");
+                throw ex;
+            }
+            
         }
 
     }
--- Glade2d/MeadowApp.cs
+++ Glade2d/MeadowApp.cs
@@ -23,13 +23,10 @@ namespace Glade2d
     // Change F7MicroV2 to F7Micro for V1.x boards
     public class MeadowApp : App<F7MicroV2, MeadowApp>
     {
-        readonly Color transparent = new Color(255, 0, 255);
-        readonly Color background = Color.CornflowerBlue;
         RgbPwmLed onboardLed;
         MicroGraphics graphics;
-        int displayWidth;
-        int displayHeight;
-        Dictionary<string, IDisplayBuffer> textures = new Dictionary<string, IDisplayBuffer>();
+
+        Renderer renderer;
 
 
         public MeadowApp()
@@ -70,20 +67,17 @@ namespace Glade2d
                 height: 240);
             LogService.Log.Trace("St7789 Graphics Device initialized.");
 
-            displayWidth = Convert.ToInt32(st7789.Width);
-            displayHeight = Convert.ToInt32(st7789.Height);
-
             graphics = new MicroGraphics(st7789);
             graphics.Rotation = RotationType._180Degrees;
-            graphics.Clear(true);
+
+            renderer = new Renderer(graphics);
+
             LogService.Log.Trace("Graphics buffer initialized.");
 
             onboardLed.SetColor(Color.Green);
 
             LogService.Log.Trace("Initialization complete.");
         }
-
-        
 
         void DrawTest()
         {
@@ -110,95 +104,21 @@ namespace Glade2d
                 elapsed = stopwatch.ElapsedMilliseconds;
                 fps = 1000f / elapsed;
                 stopwatch.Restart();
-                graphics.Clear();
-                graphics.DrawRectangle(0, 0, 240, 240, Color.CornflowerBlue, true);
-                
+
+                renderer.Clear();
 
                 for(var x = 0; x < 10; x++)
                 {
                     for(var y = 0; y < 10; y++)
                     {
-                        RenderFrame(x * frame.Width, y * frame.Height + 20, frame);
+                        renderer.RenderFrame(x * frame.Width, y * frame.Height + 20, frame);
                     }
                 }
 
-                graphics.DrawText(5, 5, fps.ToString() + "fps", Color.Black);
-                graphics.Show();
+                graphics.DrawText(5, 5, fps.ToString() + "fps", Color.White);
+                
+                renderer.DrawBuffer();
             }
-        }
-
-        void RenderBufferWithTransparency(int originX, int originY, IDisplayBuffer buffer)
-        {
-            for(var x = 0; x < buffer.Width; x++)
-            {
-                for(var y = 0; y < buffer.Height; y++)
-                {
-                    var pixel = buffer.GetPixel(x, y);
-                    if(!pixel.Equals(transparent))
-                    {
-                        graphics.DrawPixel(originX + x, originY + y, pixel);
-                    }
-                }
-            }
-        }
-
-        void RenderFrame(int originX, int originY, Frame frame)
-        {
-            if(!textures.ContainsKey(frame.TextureName))
-            {
-                LoadTexture(frame.TextureName);
-            }
-
-            var buffer = textures[frame.TextureName];
-
-            for (var x = frame.X; x < frame.X + frame.Width; x++)
-            {
-                for (var y = frame.Y; y < frame.Y + frame.Height; y++)
-                {
-                    var pixel = buffer.GetPixel(x, y);
-                    if (!pixel.Equals(transparent))
-                    {
-                        graphics.DrawPixel(originX + x, originY + y, pixel);
-                    }
-                }
-            }
-        }
-
-        void LoadTexture(string name)
-        {
-            var buffer = LoadBitmapFile(name);
-            textures.Add(name, buffer);
-        }
-
-        void UnloadTexture(string name)
-        {
-            textures.Remove(name);
-        }
-
-        IDisplayBuffer LoadBitmapResource(string name)
-        {
-            LogService.Log.Trace($"Attempting to LoadBitmapResource: {name}");
-            var resourcePath = $"Glade2d.Resources.{name}";
-            var img = Image.LoadFromResource(resourcePath);
-            return img.DisplayBuffer;
-        }
-
-        IDisplayBuffer LoadBitmapFile(string name)
-        {
-            LogService.Log.Trace($"Attempting to LoadBitmapFile: {name}");
-            var filePath = Path.Combine(MeadowOS.FileSystem.UserFileSystemRoot, name);
-            var img = Image.LoadFromFile(filePath);
-            return img.DisplayBuffer;
-        }
-
-        IDisplayBuffer LoadJpgResource(string name)
-        {
-            LogService.Log.Trace($"Attempting to LoadJpgResource: {name}");
-            var bytes = FileService.Instance.LoadResource(name);
-            var decoder = new JpegDecoder();
-            var jpg = decoder.DecodeJpeg(bytes);
-            var buffer = new BufferRgb888(decoder.Width, decoder.Height, jpg);
-            return buffer;
         }
     }
 }
