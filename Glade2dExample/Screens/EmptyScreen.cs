33f6e7911971d86b74997c78a8f5d540d31f821d
Justin Johnson <justin.d.johnson@gmail.com>
2022-07-21 16:56:25 -0600

    Added empty screen for testing FPS with no sprites

---
+++ Glade2dExample/Screens/EmptyScreen.cs
@@ -0,0 +1,17 @@
+﻿using Glade2d.Screens;
+using Glade2d.Services;
+using Meadow.Foundation;
+
+namespace Glade2dExample.Screens
+{
+    public class EmptyScreen : Screen
+    {
+        public EmptyScreen()
+        {
+            // set background color and FPS on the renderer
+            var renderer = GameService.Instance.GameInstance.Renderer;
+            renderer.ShowPerf = true;
+            renderer.BackgroundColor = Color.Cyan;
+        }
+    }
+}
