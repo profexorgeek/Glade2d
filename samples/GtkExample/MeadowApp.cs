9f8db50ec8e7cc32cfd85b35ca56daeecfbfcd73
Justin Johnson <justin.d.johnson@gmail.com>
2022-11-09 17:50:26 -0700

    Updated packages

--- samples/GtkExample/MeadowApp.cs
+++ samples/GtkExample/MeadowApp.cs
@@ -22,7 +22,7 @@ namespace MeadowApp
     {
         Game glade;
 
-        Display display;
+        Meadow.Graphics.GtkDisplay display;
 
         public override Task Initialize()
         {
@@ -30,7 +30,7 @@ namespace MeadowApp
             LogService.Log.Trace("Beginning Meadow initialization...");
 
             // initialize display device
-            display = new Display(240 * 3, 240 * 3, ColorType.Format16bppRgb565);
+            display = new GtkDisplay(240 * 3, 240 * 3, ColorType.Format16bppRgb565);
 
             // ready to go!, set LED to green
             LogService.Log.Trace("Initialization complete");
