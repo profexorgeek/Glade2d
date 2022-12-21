4acf46d3c53f45f184959650ddb6d5eac9cb2130
KallDrexx <me@mshapiro.net>
2022-12-20 20:10:12 -0500

    wip

---
+++ Glade2d/Profiling/ProfileData.cs
@@ -0,0 +1,17 @@
+﻿namespace Glade2d.Profiling;
+
+internal struct ProfileData
+{
+    private int _nextIndex;
+    private bool _rotatedAtLeastOnce;
+    private long[] _elapsedTicks;
+
+    public ProfileData(int capacity)
+    {
+        _nextIndex = 0;
+        _rotatedAtLeastOnce = false;
+        _elapsedTicks = new long[capacity];
+    }
+    
+    
+}
---
+++ Glade2d/Profiling/Profiler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Glade2d.Profiling;
+
+/// <summary>
+/// Makes it easy to track and report on performance characteristics about different operations
+/// </summary>
+public class Profiler
+{
+    private readonly Stopwatch _globalStopwatch = Stopwatch.StartNew();
+    private readonly Dictionary<string, long> _startingTicks = new();
+    private readonly Dictionary<string, List<long>> _elapsedTicks = new();
+    private bool _isActive;
+    private int _storedSampleSize = 100;
+
+    /// <summary>
+    /// Determines if the profiler should be actively tracking operations or not. If disabled most function calls
+    /// do no work.
+    /// </summary>
+    public bool IsActive
+    {
+        get => _isActive;
+        set
+        {
+            _isActive = value;
+            if (!_isActive)
+            {
+                Reset();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Resets all profiling data. Usually done when profiling is enabled or a screen change.
+    /// </summary>
+    public void Reset()
+    {
+        _startingTicks.Clear();
+        _elapsedTicks.Clear();
+    }
+}
