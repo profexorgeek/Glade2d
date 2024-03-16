6b639635d6ccf049b7216a6c26164e7aa3b45c66
dom@datti.net <dom@datti.net>
2024-03-16 00:21:05 -0400

    Everything builds now. Need to move back to using the nuget packages for the MeadowMgTestEnvironment project

--- Samples/SampleGtk/MeadowApp.cs
+++ Samples/SampleGtk/MeadowApp.cs
@@ -1,11 +1,14 @@
 ﻿using Glade2d;
+using Glade2d.Graphics.SelfRenderer;
+using Glade2d.Graphics;
+using Glade2d.Profiling;
 using Glade2d.Services;
 using GladeSampleShared.Screens;
 using Meadow;
-using Meadow.Foundation;
-using Meadow.Graphics;
+using Meadow.Foundation.Displays;
 using Meadow.Peripherals.Displays;
 using System.Threading.Tasks;
+
 
 namespace SampleGtk
 {
@@ -33,13 +36,20 @@ namespace SampleGtk
 
         public override Task Run()
         {
+            var textureManager = new TextureManager(MeadowOS.FileSystem.UserFileSystemRoot);
+            var layerManager = new LayerManager();
+            var profiler = new Profiler();
+            var renderer = new GladeSelfRenderer(display, textureManager, layerManager, profiler);
+
+            LogService.Log.Trace("Initializing Glade game engine...");
             glade = new Game();
-            glade.Initialize(display, 2, EngineMode.GameLoop);
-            glade.Renderer.RenderInSafeMode = false;
+            glade.Initialize(renderer, textureManager, layerManager, profiler);
+
+            //glade.Renderer.RenderInSafeMode = false;
             glade.SleepMilliseconds = 20;
             _ = Task.Run(() =>
             {
-                glade.Start(new GladeDemoScreen());
+                glade.Start(() => new GladeDemoScreen());
             });
             display.Run();
 
